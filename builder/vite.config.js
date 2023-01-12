66e00fdfabe7b319fa0a579e55931b5a0172f066
Manuel Feller (BI X) <manuel.feller@boehringer-ingelheim.com>
2023-01-12 12:45:35 +0100

    moving inputs masks into partials

--- builder/vite.config.js
+++ builder/vite.config.js
@@ -1,6 +1,13 @@
+import { resolve } from 'path';
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-	plugins: [viteSingleFile()],
+	plugins: [
+		viteSingleFile(),
+		handlebars({
+      partialDirectory: resolve(__dirname, 'fragments'),
+    }),
+	],
 })
